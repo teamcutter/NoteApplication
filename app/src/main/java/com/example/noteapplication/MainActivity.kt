500423dc0f8e5a823a2a0dfe480e972ca3867fc7
ruslan.sibgatulin05@gmail.com <ruslan.sibgatulin05@gmail.com>
2021-12-16 22:39:11 +0600

    spinner

--- app/src/main/java/com/example/noteapplication/MainActivity.kt
+++ app/src/main/java/com/example/noteapplication/MainActivity.kt
@@ -2,6 +2,10 @@ package com.example.noteapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Toast
 import com.example.noteapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,5 +16,19 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val customList = listOf("First", "Second", "Third", "Fourth")
+        val adapter = ArrayAdapter(this, R.layout.support_simple_spinner_dropdown_item, customList)
+        binding.spMonths.adapter = adapter
+        binding.spMonths.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(adapterView: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                Toast.makeText(this@MainActivity,
+                "You selected ${adapterView?.getItemAtPosition(position).toString()}",
+                Toast.LENGTH_LONG).show()
+            }
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+
+            }
+        }
     }
 }
